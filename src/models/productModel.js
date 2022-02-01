769e349ef42bb84cd2787478b19e189d1500a589
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-02-01 11:51:23 +0200

    add Product

---
+++ src/models/productModel.js
@@ -0,0 +1,39 @@
+const Product = (database) => {
+    const Sequelize = database.Sequelize;
+    return database.define('product', {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true,
+        },
+        merchant_id: {
+            type: Sequelize.INTEGER,
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: Sequelize.STRING,
+        },
+        price: {
+            type: Sequelize.DECIMAL(10, 2),
+        },
+        category: {
+            type: Sequelize.STRING,
+        },
+        image: {
+            type: Sequelize.STRING,
+        },
+        quantity: {
+            type: Sequelize.INTEGER,
+        },
+        unit_type: {
+            type: Sequelize.STRING,
+        },
+        status: {
+            type: Sequelize.STRING,
+        },
+     });
+}
