e6b5ad22bfcce330ebd7ae3c9d4f5753a168e29e
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-02-09 16:37:24 +0200

    update

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ dotenv.config({
 });
 
 
-
 const userRoutes = require('./routes/userRoutes');
 const exhibitorRoutes = require('./routes/exhibitorRoutes');
 const globalErrHandler = require('./controllers/errorController');
