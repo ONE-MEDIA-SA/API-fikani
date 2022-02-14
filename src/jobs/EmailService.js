d5a12cf7ce8c2f50b8b98a7431e2da0275de7860
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-02-14 13:55:16 +0200

    update

--- src/jobs/EmailService.js
+++ src/jobs/EmailService.js
@@ -13,13 +13,21 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
  * @to - 
  */
 function emailService(to, message, subject) {
+    // const msg = {
+    //     to: this.to, // Change to your recipient
+    //     from: 'agostinhodossantos27@gmail.com', // Change to your verified sender
+    //     subject: this.subject,
+    //     html: this.message,
+    // } 
     const msg = {
-        to: this.to, // Change to your recipient
+        to: 'agostinhodev@gmail.com', // Change to your recipient
         from: 'agostinhodossantos27@gmail.com', // Change to your verified sender
-        subject: this.subject,
-        html: this.message,
+        subject: "test",
+        html: `<h1> Hello world </h1>`,
     } 
-    emailQueue.add(send(msg))   
+
+    send(msg)
+    //emailQueue.add(send(msg))   
 }
 
 function send(msg) {
